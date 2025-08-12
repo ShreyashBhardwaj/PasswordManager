23b37ee5700f1ae39673a0334ecce7a71b40c215
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-12 20:33:31 +0530

    Now basic stuff done! Now to work on the encryption

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const port = 3001;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
   user: "root",
@@ -10,7 +14,22 @@ const db = mysql.createConnection({
   database: "passwordmanager",
 });
 
-app.get("/addpassword", (req, res) => {});
+app.post("/addpassword", (req, res) => {
+  const { passwords, title } = req.body;
+  db.query(
+    "Insert into passwords (password,website) values (?, ?)",
+    [passwords, title],
+    (err, result) => {
+      if (err) {
+        console.error("Error inserting password:", err);
+        res.status(500).send("Error inserting password");
+      } else {
+        console.log("Password added successfully");
+        res.status(200).send("Password added successfully");
+      }
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
