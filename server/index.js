bae0d906743d0cb96012e74cd2e21695fc639abc
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-12 18:16:35 +0530

    Made the frontend

--- server/index.js
+++ server/index.js
@@ -6,13 +6,11 @@ const port = 3001;
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "(Sanat123$%^)",
+  password: "(Sanat123$%^)", // Replace with your actual password
   database: "passwordmanager",
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.get("/addpassword", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
