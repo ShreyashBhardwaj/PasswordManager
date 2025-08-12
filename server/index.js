cf19e5243ff93c4c40f35186a94a485a87ea889a
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-12 17:51:57 +0530

    Going to create the client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 const port = 3001;
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "(Sanat123$%^)",
+  database: "passwordmanager",
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
